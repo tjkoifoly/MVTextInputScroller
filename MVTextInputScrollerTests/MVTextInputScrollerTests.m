062caa40e8dcd3a4bde5b428db6f5ff7495026d9
Andrea Bizzotto <andrea@snupps.com>
2014-03-25 13:24:12 +0000

    Initial Commit

---
+++ MVTextInputScrollerTests/MVTextInputScrollerTests.m
@@ -0,0 +1,34 @@
+//
+//  MVTextInputScrollerTests.m
+//  MVTextInputScrollerTests
+//
+//  Created by Andrea Bizzotto on 25/03/2014.
+//  Copyright (c) 2014 musevisions. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MVTextInputScrollerTests : XCTestCase
+
+@end
+
+@implementation MVTextInputScrollerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
