8f9d450cc5e142f57a7f1c0041e1bb207c40a14d
Andrea Bizzotto <andrea@snupps.com>
2014-04-15 23:02:35 +0100

    Using added pod dependencies.

--- MVTextInputScroller/Classes/ViewController.m
+++ MVTextInputScroller/Classes/ViewController.m
@@ -1,13 +1,17 @@
-//
-//  ViewController.m
-//  MVTextInputScroller
-//
-//  Created by Andrea Bizzotto on 25/03/2014.
-//  Copyright (c) 2014 musevisions. All rights reserved.
-//
+/*
+ ViewController.m
+ Copyright (c) 2014 Andrea Bizzotto bizz84@gmail.com
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 #import "ViewController.h"
 #import "MVTextInputsScroller.h"
+#import "UITextView+MVHeight.h"
 
 @interface ViewController()<UITextFieldDelegate, UITextViewDelegate>
 @property(strong, nonatomic) MVTextInputsScroller *inputsScroller;
@@ -27,14 +31,25 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
+    self.bioInput.delegate = self;
+
     self.inputsScroller = [[MVTextInputsScroller alloc] initWithScrollView:self.scrollView];
     self.scrollView.keyboardDismissMode = UIScrollViewKeyboardDismissModeOnDrag;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setInputResponderChain {
+
+    NSArray *textFields = @[self.nameInput, self.surnameInput, self.postcodeInput, self.addressL1Input, self.addressL2Input];
+    for (UITextField *textField in textFields) {
+        textField.returnKeyType = UIReturnKeyNext;
+    }
+    
+}
+
+#pragma mark - UITextViewDelegate
+- (void)textViewDidChange:(UITextView *)textView {
+
+    //[textView adjustHeightIfNeeded];
 }
 
 
